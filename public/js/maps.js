1ac2dfed430cae42290aab7f82018e117aed269d
alexanderqchen <aqchen@ucla.edu>
2018-03-31 15:46:01 -0700

    basic frontend for map done

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,7 +1,15 @@
+const searchBar = document.getElementById("search");
+
 function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        zoom: 3
     });
 }
+
+$("#submit").on('click', (e) => {
+    e.preventDefault();
+    var movieTitle = searchBar.value;
+    console.log(movieTitle);
+});
