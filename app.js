b14a1354a00d859e462fe1b5a8be49d8ce8ebe25
alexanderqchen <aqchen@ucla.edu>
2018-03-31 20:38:23 -0700

    did pretty much nothing

--- app.js
+++ app.js
@@ -26,21 +26,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //request to myapifilms directly
-
 function getMovieID(movieTitle) {
 	request('http://www.myapifilms.com/imdb/idIMDB?title=' + movieTitle + '&token=192453f5-4f1d-49cb-91d4-e0cb991ab10f&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=2&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=0&biography=0&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=0&actorTrivia=0&similarMovies=0&adultSearch=0&goofs=0&keyword=0&quotes=0&fullSize=0&companyCredits=0&filmingLocations=2', (error, response, data) => {
-	if(error) {
-		console.log("Error when calling myapifilms api.");
-	}
-	else {
-		data = JSON.parse(data);
-		var movieTitle = data.data.movies[0].title;
-		console.log(movieTitle);
+		if(error != null) {
+			console.log(error);
+		}
+		else {
+			data = JSON.parse(data);
+			var movieTitle = data.data.movies[0].title;
+			console.log(movieTitle);
 
-		var id = data.data.movies[0].idIMDB;
-		console.log(id);
-	}
-});
+			var id = data.data.movies[0].idIMDB;
+			console.log(id);
+		}
+	});
 }
 
 //occurs when website is loaded
